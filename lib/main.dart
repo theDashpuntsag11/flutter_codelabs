227b78d1d6d5756c6af3bdccaf5ce3f967873de0
theDashpuntsag11 <thedashpuntsag@gmail.com>
2022-11-15 12:02:51 +0800

    documenting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+// ignore_for_file: slash_for_doc_comments
+
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'package:startup_namer/01_randomWord/app.dart';
+
+/**
+ *          Project list
+ *      1. RandomWordApp 
+ *          + Generates pairs of word and shows them by list. 
+ *          + Wordpairs can be added or removed form favorite list. * 
+ * 
+ */
 
 void main() {
   simpleRunApp();
